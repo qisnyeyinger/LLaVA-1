2fa4f8147c6b81a6b5ad093e9ad14d6e65f0d80e
Qisnyeyinger <chenwk25@gmail.com>
2026-01-22 16:45:02 +0800

    update eval

--- scripts/v1_5/eval/textvqa.sh
+++ scripts/v1_5/eval/textvqa.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
-python -m llava.eval.model_vqa_loader \
-    --model-path /data1/cwk/mllm/models/llava-v1.5-7b \
-    --question-file ./playground/data/eval/textvqa/llava_textvqa_val_v051_ocr.jsonl \
-    --image-folder /data1/cwk/mllm/datasets/textvqa/train_images \
-    --answers-file ./playground/data/eval/textvqa/answers/llava-v1.5-7b.jsonl \
-    --temperature 0 \
-    --conv-mode vicuna_v1
+CUDA_VISIBLE_DEVICES=7
+# python -m llava.eval.model_vqa_loader \
+#     --model-path /data1/cwk/mllm/models/llava-v1.5-7b \
+#     --question-file ./playground/data/eval/textvqa/llava_textvqa_val_v051_ocr.jsonl \
+#     --image-folder /data1/cwk/mllm/datasets/textvqa/train_images \
+#     --answers-file ./playground/data/eval/textvqa/answers/llava-v1.5-7b.jsonl \
+#     --temperature 0 \
+#     --conv-mode vicuna_v1
 
 python -m llava.eval.eval_textvqa \
     --annotation-file ./playground/data/eval/textvqa/TextVQA_0.5.1_val.json \
